39c0dc3ec88f88a492baed8a9bd6b6f1ba1304fa
frank junior reyes <heatfrank02@gmail.com>
2023-12-02 17:55:47 -0400

    Add project files.

---
+++ Tarea Colores/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tarea_Colores.Models;
+
+namespace Tarea_Colores
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<ColorModel> Colors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=colors.db");
+        }
+    }
+}
---
+++ Tarea Colores/Models/ColorModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Tarea_Colores.Models
+{
+    public class ColorModel
+    {
+        public int Id { get; set; }
+        public string Valor { get; set; }
+    }
+}
